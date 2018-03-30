e94b82273570d60cdf60e3f3f8435a6b5b8766d4
perssondan <dan.persson@cenito.se>
2018-03-30 12:59:52 +0200

    Added menu
    Adding support for non-standard menu
    Support for creating extra window

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron'); // Import electron module
 const url = require('url'); // Import url module
 const path = require('path'); // Import path module
 
-const {app, BrowserWindow} = electron; // Grab app and BrowserWindow from electron
+const {app, BrowserWindow, Menu} = electron; // Grab app and BrowserWindow from electron
 
 let mainWindow; // Create variable the mainWindow
+let addWindow; // Create variable the addWindow
 
 // Listen for app to be ready
 app.on('ready', function (){
@@ -16,5 +17,83 @@ app.on('ready', function (){
         protocol: 'file',
         slashes: true
     }));
+
+    // Quit app when closed
+    mainWindow.on('closed', function() {
+        app.quit();
+    });
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
 
+// Handle create add window
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add shopping list item'
+    });
+    // Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    // Garbage collection handler
+    addWindow.on('close', function() {
+        addWindow = null;
+    });
+}
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label: 'Add Item',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// if mac, add empty object to menu
+if (process.platform == 'darwin') {
+    // unshift, inserting
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in production
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
